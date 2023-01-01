372363bee44412c162bf06d569fecdf8fa4f3c7b
Antus Balázs <antus.balazs@avander.hu>
2023-01-01 16:42:01 +0100

    fe - navigation, welcome page

--- hirportal/frontend/src/app/app.module.ts
+++ hirportal/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,6 +38,7 @@ import { NewsComponent } from './news/news.component';
     MatInputModule,
     MatListModule,
     MatToolbarModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
