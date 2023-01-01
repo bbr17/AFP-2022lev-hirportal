c41ddf0da8b1e46197ccaa57a849c5caeb5f9687
Antus Balázs <antus.balazs@avander.hu>
2023-01-01 16:58:36 +0100

    fe - register page init, fixed nav error

--- hirportal/frontend/src/app/register/register.component.ts
+++ hirportal/frontend/src/app/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = this.initFormGroup();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // this.registerForm = this.initFormGroup();
   }
 
+  initFormGroup(): FormGroup {
+    return new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  register(): void {}
 }
